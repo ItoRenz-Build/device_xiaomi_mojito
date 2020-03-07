4276aac8afb8c408f1da10cc34242b1facc44cc5
Wei Wang <wvw@google.com>
2020-03-06 16:49:16 -0800

    mojito: import power-libperfmgr and libperfmgr
    * From hardware/google/pixel (android-11.0.0_r35)
    * Powerhint from sunfish (android-11.0.0_r35)
    
    Change-Id: Ie037f55bfbef59451ea1634d5cdf36a25c091736
    Signed-off-by: daniml3 <daniel@danielml.dev>

--- device.mk
+++ device.mk
@@ -348,8 +348,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2 \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.mojito-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
 PRODUCT_PACKAGES += \
