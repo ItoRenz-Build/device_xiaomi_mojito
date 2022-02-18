f2c0a699dad11044634da16b4ad1effd64b6a847
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-18 15:34:06 +0700

    mojito: Don't override DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE
    - Allow device-specific manifests to be safely added
    - It's can't find framework manifest vendor.power.aospa hal it's because we use manifest lv.5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,7 +95,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
