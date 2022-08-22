1030646abe838a5e4547793779adc68792fb8cfa
ItoRenz <itorenz@nusantararom.org>
2022-08-22 14:55:56 +0700

    mojito: Nusantara bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

---
+++ nad_mojito.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mojito device
+$(call inherit-product, device/xiaomi/mojito/device.mk)
+
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Product Specifics
+PRODUCT_NAME := nad_mojito
+PRODUCT_DEVICE := mojito
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Nusantara Stuff
+NAD_BUILD_TYPE := OFFICIAL
+USE_GAPPS := true
+
+# Some Build Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_USES_AOSP_CHARGER := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
