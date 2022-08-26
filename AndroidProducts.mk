8e51b110d9487a55a3571bc4e4a126ccc5d0642f
ItoRenz <itorenz@nusantararom.org>
2022-08-27 06:41:25 +0700

    mojito: Ancient bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_mojito.mk
+    $(LOCAL_DIR)/ancient_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_mojito-user \
-    nad_mojito-userdebug \
-    nad_mojito-eng
+    ancient_mojito-user \
+    ancient_mojito-userdebug \
+    ancient_mojito-eng
--- ancient_mojito.mk
+++ ancient_mojito.mk
@@ -11,25 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common Ancient stuff.
+$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := nad_mojito
+PRODUCT_NAME := ancient_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Nusantara Stuff
-NAD_BUILD_TYPE := OFFICIAL
-USE_GAPPS := true
+# Ancient Stuff
+ANCIENT_OFFICIAL := true
+ANCIENT_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := true
-TARGET_USES_AOSP_CHARGER := true
+USE_PIXEL_CHARGING := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := false
